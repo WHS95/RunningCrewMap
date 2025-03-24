0c47aaf194e423dfd25288d3a5110c4b0f11195f
WHS95 <tjdngur22@gmail.com>
2025-03-25 00:30:50 +0900

    ♻️ 네이버 최대 줌 아웃 대한민국 레벨 까지

--- src/components/map/NaverMap.tsx
+++ src/components/map/NaverMap.tsx
@@ -333,7 +333,7 @@ export default function NaverMap({
           initialCenter.lng
         ),
         zoom: initialZoom,
-        minZoom: 9,
+        minZoom: 7,
         maxZoom: 21,
         zoomControl: false,
         renderOptions: {
